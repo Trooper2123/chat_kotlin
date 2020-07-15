be89362b2712bf7f671323e8cc230a410c7a01a8
tlbn <tete.leticia@gmail.com>
2020-07-15 11:09:16 -0300

    heroku

---
+++ src/test/kotlin/com/chat/chat_kotlin/ChatKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chat.chat_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
